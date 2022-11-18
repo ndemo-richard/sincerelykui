aa4482a8afa59cf26b3fedb6ce1eb9dc006c2cfe
ndemo-richard <ndemorich@gmail.com>
2022-11-18 15:02:12 +0300

    update brand color

--- front-end/.next/static/chunks/webpack.js
+++ front-end/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0aafec0011d426a2"; }
+/******/ 		__webpack_require__.h = function() { return "da2dc23386857cff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
