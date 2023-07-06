ad8a0d166ff3291f3a9e31a729ca94b2f9c677a8
ndemo-richard <ndemorich@gmail.com>
2023-07-06 07:52:28 +0300

    updated aboutme

--- front-end/pages/about.js
+++ front-end/pages/about.js
@@ -19,10 +19,16 @@ const about =()=>(
         <div className={styles.body_wrapper}>
         <div className={styles.mainImage_intro}><img  src='/kui.jpeg' alt="kui photo"  /></div>
         <div className={styles.body_content}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla in pellentesque eleifend, urna metus interdum turpis, et egestas dui justo vel neque. Nam ut nisl in nunc mattis mollis. Curabitur blandit velit sit amet diam volutpat, quis placerat libero cursus. Pellentesque pellentesque eget ipsum eu egestas. Maecenas elit nulla, luctus et quam ut, rhoncus sagittis massa. Nullam nec elementum dui. Fusce accumsan dictum tempor. In hac habitasse platea dictumst. Quisque ac bibendum sem. Curabitur ornare sagittis mauris nec cursus. Vivamus sagittis blandit tincidunt. Nunc pharetra nec sem tincidunt scelerisque.</p>
-            <p>Ut eu turpis in magna bibendum suscipit. Sed hendrerit rutrum dolor eget ornare. Pellentesque nec porta felis. Pellentesque viverra feugiat vulputate. Proin eros velit, egestas non eleifend ut, euismod vitae leo. Vivamus feugiat auctor ligula. Nullam tincidunt lorem ac urna efficitur porttitor. Phasellus cursus efficitur sodales. Suspendisse et tempus mauris</p>
-            <p>Donec hendrerit arcu ut nisi gravida tempor. Praesent vitae nibh eu felis dictum commodo. Nulla urna velit, maximus at dui ut, posuere maximus velit. Aenean feugiat varius augue vestibulum faucibus</p>
-            <p>Nulla facilisi. Donec ullamcorper tempor ipsum, sit amet aliquet turpis tempus eu. Curabitur in efficitur dui, in dictum ipsum. Fusce commodo maximus commodo. Suspendisse auctor libero in elementum fermentum. Aliquam at tellus vel sem facilisis finibus non ac ligula. Nullam tortor leo, sodales in consectetur ut, sollicitudin consequat velit</p>
+           <p>Heey!</p>
+<p>I’m Kui</p>
+<p>I’m a psychologist, writer, coffee lover and obsessed with this Turkish series ‘’love for rent’’.</p>
+<p>Living in a world that most often exerts pressure on us to ‘’fit in’’ can be overwhelming and many like
+me are sucked into believing that we actually need to ‘’fit in’’ to feel like we belong. So, what will
+happen if we actually be OURSELVES instead of trying so hard to ‘’fit in’’ in a world that is constantly
+trying to make us something else?</p>
+<p>I’m here to help you become FEARLESS AUTHENTIC; to be yourself and live a life that not only looks
+good on the outside but also feels good on the inside. To be REAL, be true to your values regardless of
+what the society and others think or expect of you!</p>
         </div>
         </div>
       </section>
