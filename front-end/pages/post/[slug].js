39ae4edf2dc28abfe4fc773fbe1a5eda38f2dd23
ndemo-richard <ndemorich@gmail.com>
2023-07-08 20:43:07 +0300

    update

--- front-end/pages/post/[slug].js
+++ front-end/pages/post/[slug].js
@@ -37,6 +37,10 @@ export const Post = ({ _id, title, body, image,publishedAt, authorImage, authorN
             <Head>
                 <title>{title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+              
+<link rel="preload" href="https://cdn.shareaholic.net/assets/pub/shareaholic.js" as="script" />
+<meta name="shareaholic:site_id" content="9f73a847a153623e7f0b09b7c585bd88" />
+<script data-cfasync="false" async src="https://cdn.shareaholic.net/assets/pub/shareaholic.js"></script>
 
             </Head>
         <div className={styles.container}>
