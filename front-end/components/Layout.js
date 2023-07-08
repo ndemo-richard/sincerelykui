3a3f45f6086bef4f4d967c11f1fcfafef88ff064
ndemo-richard <ndemorich@gmail.com>
2023-07-08 21:01:45 +0300

    update

--- front-end/components/Layout.js
+++ front-end/components/Layout.js
@@ -13,6 +13,10 @@ const Layout = props =>{
             <Head>
             <title>sincerelykui</title>
 	  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    
+<link rel="preload" href="https://cdn.shareaholic.net/assets/pub/shareaholic.js" as="script" />
+<meta name="shareaholic:site_id" content="9f73a847a153623e7f0b09b7c585bd88" />
+<script data-cfasync="false" async src="https://cdn.shareaholic.net/assets/pub/shareaholic.js"></script>
 
             </Head>
          
